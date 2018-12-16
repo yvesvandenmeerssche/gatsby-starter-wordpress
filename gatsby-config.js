4116dd249f78f0e6d370750f61f11ece8cc7b5b1
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-16 09:42:54 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,45 @@ module.exports = {
         htaccess_user: "jamstack",
           htaccess_pass: "#80Bm%roSb",
           htaccess_sendImmediately: false,
+        // plugin, you can specify user and password to obtain access token and use authenticated requests against wordpress REST API.
+          jwt_user: process.env.JWT_USER,
+          jwt_pass: process.env.JWT_PASSWORD,
+        },
+        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
+        // It can help you debug specific API Endpoints problems.
+        verboseOutput: false,
+        // Set how many pages are retrieved per API request.
+        perPage: 100,
+        // Search and Replace Urls across WordPress content.
+        searchAndReplaceContentUrls: {
+          sourceUrl: "https://surfbnb.tv/jamstack/",
+          replacementUrl: "https://jamstacklivethelifetv.netlify.com",
+        },
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 10,
+        // Set WP REST API routes whitelists
+        // and blacklists using glob patterns.
+        // Defaults to whitelist the routes shown
+        // in the example below.
+        // See: https://github.com/isaacs/minimatch
+        // Example:  `["/*/*/comments", "/yoast/**"]`
+        // ` will either include or exclude routes ending in `comments` and
+        // all routes that begin with `yoast` from fetch.
+        // Whitelisted routes using glob patterns
+        includedRoutes: [
+          "/*/*/categories",
+          "/*/*/posts",
+          "/*/*/pages",
+          "/*/*/media",
+          "/*/*/tags",
+          "/*/*/taxonomies",
+          "/*/*/users",
+        ],
+        // Blacklisted routes using glob patterns
+        excludedRoutes: ["/*/*/posts/1456"],
+        // use a custom normalizer which is applied after the built-in ones.
+        normalizer: function({ entities }) {
+          return entities
         },
         // Set to true to debug endpoints on 'gatsby build'
         verboseOutput: false,
